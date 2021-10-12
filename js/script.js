880593f5e56e039882fe6f1d045b0c0aa8649664
EddieFreedome <eddie.freed@tutanota.com>
2021-10-12 16:17:09 +0200

    Tentativo creazione quadrati

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
-const containerGrid = document.getElementById("containerGrid")
+const containerGrid = document.querySelector(".container")
 //let square = document.querySelector(".my-col")
 
 for (let i = 1; i <= 100 ; i++) {
     //genero col
     //attacco col
     const square = document.createElement("div")
-    square.classList.add (".my-col")
+    square.classList.add ("my-col")
     
     if (i % 3 === 0)  {
-        
+        square.classList.add(".x3")
     } else if (i % 5 === 0)  {
-        
+        square.classList.add(".x5")
     } else if (i % 3 === 0 && i % 5 === 0)  {
-        
+        square.classList.add(".x3-5")
+    } else {
+        square.classList.add(".std")
     }
-    
+containerGrid.append(square) 
 }
