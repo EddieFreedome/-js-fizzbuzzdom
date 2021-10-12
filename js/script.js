a50e03d00e8b39fd2be6ee72cb158fe82a1a2784
EddieFreedome <eddie.freed@tutanota.com>
2021-10-12 15:55:01 +0200

    Proseguito esercizio

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+const containerGrid = document.getElementById("containerGrid")
+//let square = document.querySelector(".my-col")
+
 for (let i = 1; i <= 100 ; i++) {
-    console.log(i);
+    //genero col
+    //attacco col
+    const square = document.createElement("div")
+    square.classList.add (".my-col")
+    
+    if (i % 3 === 0)  {
+        
+    } else if (i % 5 === 0)  {
+        
+    } else if (i % 3 === 0 && i % 5 === 0)  {
+        
+    }
+    
 }
