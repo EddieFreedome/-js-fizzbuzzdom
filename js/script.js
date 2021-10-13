48aad736b08f8761429ad4be404b87ed65fca346
EddieFreedome <eddie.freed@tutanota.com>
2021-10-13 08:13:21 +0200

    corretto esercizio e completato

--- js/script.js
+++ js/script.js
@@ -6,16 +6,19 @@ for (let i = 1; i <= 100 ; i++) {
     //attacco col
     const square = document.createElement("div")
     square.classList.add ("my-col")
-    
     if (i % 3 === 0 && i % 5 === 0)  {
         square.classList.add("x3-5")
+        square.innerHTML = "FizzBuzz"
     } else if (i % 5 === 0)  {
         square.classList.add("x5")
+        square.innerHTML = "Buzz"
     } else if (i % 3 === 0)  {
         square.classList.add("x3")
+        square.innerHTML = "Fizz"
     } else {
         square.classList.add("std")
+        square.innerHTML = i
     }
-    
+
 containerGrid.append(square) 
 }
